cc5e8c6f0ddd3e030d8ff52900d11762627dd65c
Stijn Verhoeven <stijnv1@hotmail.com>
2015-12-07 16:25:11 +0100

    added set-ipdnssettings function to set new DNS ip addresses on NIC interfaces

--- DNSRelatedScripts/DNSRelatedScripts/CheckandUpdateDNSSettings.ps1
+++ DNSRelatedScripts/DNSRelatedScripts/CheckandUpdateDNSSettings.ps1
@@ -39,7 +39,7 @@ function Get-IPDNSSettings ($DiscoveredServers, $FileLogPath)
 				#server name is discovered in DNS, test whether server is online by testing connection to WinRM port
 				Write-Host "Server $($server.Name) is found in DNS, checking net connections ..." -ForegroundColor Yellow
 
-				if (Test-NetConnection -ComputerName $server.Name -CommonTCPPort WINRM -InformationLevel Quiet -ErrorAction SilentlyContinue)
+				if ((Test-NetConnection -ComputerName $server.Name -CommonTCPPort WINRM -InformationLevel Quiet -ErrorAction SilentlyContinue) -and ($server.Description -ne "Failover cluster virtual network name account"))
 				{
 					Write-Host "$($server.Name) is online, getting DNS client settings ..." -ForegroundColor Yellow
 					#start discovery of DNS client settings on this server
@@ -54,24 +54,24 @@ function Get-IPDNSSettings ($DiscoveredServers, $FileLogPath)
 					$NetItems = @(Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter "IPEnabled = 'True'" -ComputerName $StrComputer)
 					$intRowNet = 0
 					$ServerObj | Add-Member -MemberType NoteProperty -Name "NIC's" -Value $NetItems.Length -Force
+
+					#initialize string arrays
 					[STRING]$MACAddresses = @()
 					[STRING]$IpAddresses = @()
 					[STRING]$DNS = @()
 					[STRING]$DNSSuffix = @()
+					[STRING]$NICIndex = @()
+
 					foreach ($objItem in $NetItems)
 					{
 						#check if interface has DNS servers configured. If not, interface is not added to the array
-						if ($objItem.{DNSServerSearchOrder}.Count -gt 0)
+						if (($objItem.{DNSServerSearchOrder}.Count -gt 0) -and ($objItem.Description -ne "Microsoft Failover Cluster Virtual Adapter") -and ($objItem.IPAddress -notlike "0.0.0.0*"))
 						{
 							if ($objItem.IPAddress.Count -gt 1)
 							{
-								#check if interface has DNS servers configured
-								if ($objItem.{DNSServerSearchOrder}.Count -gt 0)
-								{
-									$TempIpAdderesses = [STRING]$objItem.IPAddress
-									$TempIpAdderesses  = $TempIpAdderesses.Trim().Replace(" ", " - ")
-									$IpAddresses += $TempIpAdderesses
-								}
+								$TempIpAdderesses = [STRING]$objItem.IPAddress
+								$TempIpAdderesses  = $TempIpAdderesses.Trim().Replace(" ", " - ")
+								$IpAddresses += $TempIpAdderesses
 							}
 							else
 							{
@@ -111,6 +111,8 @@ function Get-IPDNSSettings ($DiscoveredServers, $FileLogPath)
 								$DNSSuffix += $objItem.DNSDomainSuffixSearchOrder +"- "
 							}
 
+							$NICIndex += $objItem.Index
+
 							$SubNet = [STRING]$objItem.IPSubnet[0]
 							$intRowNet = $intRowNet + 1
 						}
@@ -128,11 +130,12 @@ function Get-IPDNSSettings ($DiscoveredServers, $FileLogPath)
 					$ServerObj | Add-Member @Member -Name "DHCP Lease Obtained" -Value $objItem.DHCPLeaseObtained
 					$ServerObj | Add-Member @Member -Name "DHCP Lease Expires" -Value $objItem.DHCPLeaseExpires
 					$ServerObj | Add-Member @Member -Name "Operating System" -Value $server.OperatingSystem
+					$ServerObj | Add-Member @Member -Name "NICIndex" -Value $NICIndex
 					$AllServers += $ServerObj
 				}
 				else
 				{
-					Write-Host "Net connection test for server $($server.Name) failed." -ForegroundColor Cyan
+					Write-Host "Net connection test for server $($server.Name) failed or server object is a Window Server cluster object." -ForegroundColor Cyan
 
 					#write to log that no info is gathered of this server
 					$WriteToLog = "No information can be gathered from server $($server.Name). Net Connection test failed"
@@ -160,10 +163,31 @@ function Get-IPDNSSettings ($DiscoveredServers, $FileLogPath)
 	return $AllServers
 }
 
-#create inventory of windows servers
-$Servers = Get-ADComputer -SearchBase "$OUDistinguishedName" -Filter {operatingSystem -like "Windows Server*"} -Properties operatingSystem
+function Set-IPDNSSettings ($DiscoveredServers, $DNSServers, $LogPath)
+{
+	foreach ($server in $DiscoveredServers)
+	{
+		try
+		{
+			Write-Host "Update DNS settings on server $($server.HostName) ..." -ForegroundColor Green
 
-if ($Servers.Count -gt 0)
+			$wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true' AND Index = '$($server.NICIndex)'" -ComputerName $server.Hostname
+			$wmi.SetDNSServerSearchOrder($DNSServers)
+		}
+
+		catch [Exception]
+		{
+			$WriteToLog = "Error occured in function Set-IPDNSSettings during processing of server $($server.HostName).`n $_"
+			Add-Content -Path $LogPath -Value $WriteToLog
+			$ErrorActionPreference = "SilentlyContinue"
+		}
+	}
+}
+
+#create inventory of windows servers, only active computer objects are queried
+$Servers = Get-ADComputer -SearchBase "$OUDistinguishedName" -SearchScope Subtree -Filter {(operatingSystem -like "Windows Server*") -and (Enabled -eq $true)} -Properties operatingSystem,Description 
+
+if ($Servers)
 {
 	$IPSettings = Get-IPDNSSettings -DiscoveredServers $Servers -FileLogPath $LogPath
 }
@@ -191,9 +215,54 @@ if ($IPSettings -ne $null)
 
 	$WriteCountAD = "Servers found in Active Directory = $($Servers.Count)"
 	$WriteCountDNSArray = "Number of servers of which the DNS settings are discovered = $($IPSettings.Count)"
-	Write-Host $WriteCountAD
-	Write-Host $WriteCountDNSArray
-
 	Add-Content -Value $WriteCountAD -Path $LogPath
 	Add-Content -Value $WriteCountDNSArray -Path $LogPath
 }
+
+#region Ask for input: change DNS settings or not
+$title = "Change DNS Settings"
+$message = "Do you want to change the DNS settings of the discovered servers?"
+
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+    "Change DNS Settings"
+
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+    "Do not change DNS Settings"
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+switch ($result)
+{
+	0 #DNS settings must be updated
+	{
+		#DNS settings are updated. First, input is asked for the new DNS IP settings
+		$stopAskingDNS = $false
+		$DNSIPs = @()
+		do
+		{
+			$DNSIP = Read-Host "Give DNS Server IP (blank if all DNS servers are given)"
+			if ($DNSIP)
+			{
+				$DNSIPs += $DNSIP
+			}
+			else
+			{
+				$stopAskingDNS = $true
+			}
+
+		} while (!$stopAskingDNS)
+
+		#start update of DNS settings using function
+		Write-Host "Start updating DNS server settings ..." -ForegroundColor Yellow
+		Set-IPDNSSettings -DiscoveredServers $IPSettings -DNSServers $DNSIPs -LogPath $LogPath
+	}
+
+	1 {"You selected No."}
+}
+#endregion
+Write-Host $WriteCountAD
+Write-Host $WriteCountDNSArray
+
+
