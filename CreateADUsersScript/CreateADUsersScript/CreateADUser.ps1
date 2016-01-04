cb803fc71e25c14fd9ec3d6303d3277a79808b81
Stijn Verhoeven <stijnv1@hotmail.com>
2016-01-04 16:18:16 +0100

    work in progress

--- CreateADUsersScript/CreateADUsersScript/CreateADUser.ps1
+++ CreateADUsersScript/CreateADUsersScript/CreateADUser.ps1
@@ -4,13 +4,41 @@
 param
 (
 	[Parameter(Mandatory=$true)]
-	[string[]]$CSVPath
+	[string[]]$CSVPath,
+
+	[Parameter(Mandatory=$true)]
+	[string[]]$OUDistinguishedName
 )
+
+Function CreateADUser
+{
+	param
+	(
+		[string]$UserGivenName,
+		[string]$UserSurName,
+		[string]$UserSamAccountName,
+		[string]$UserUPN,
+		[string]$UserPassword,
+		[string]$OU
+	)
+
+	#convert password to securestring
+	$passwordSec = ConvertTo-SecureString $UserPassword -AsPlainText -Force
+
+	#create user in correct OU
+	$createdUserObject = New-ADUser 
+}
 
 Try
 {
 	#import CSV data
+	$UserData = Import-Csv $CSVPath
 
+	#start looping through user data and create objects
+	foreach ($userObject in $UserData)
+	{
+
+	}
 }
 Catch
 {
